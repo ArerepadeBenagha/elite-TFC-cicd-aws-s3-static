ef01c39eb8b2e988241dfcef21a17cc43ada0896
ArerepadeB <aredelulu@gmail.com>
2021-10-30 08:46:04 -0500

    tfc setup for cicd

--- development/provider.tf
+++ development/provider.tf
@@ -8,8 +8,14 @@ terraform {
   }
 }
 provider "aws" {
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  region     = var.aws_region
 }
 
 provider "aws" {
   alias      = "acm_provider"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  region     = var.aws_region
 }
--- development/variables.tf
+++ development/variables.tf
@@ -6,3 +6,8 @@ variable "bucket_name" {
   type        = string
   description = "The name of the bucket without the www. prefix. Normally domain_name."
 }
+
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+
+variable "aws_region" {}
