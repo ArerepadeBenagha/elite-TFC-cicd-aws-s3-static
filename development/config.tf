b19bd1b2ac9ba6c4d866b4875ddec47f97619d99
ArerepadeB <aredelulu@gmail.com>
2021-10-30 08:42:16 -0500

    tfc setup for cicd

--- development/config.tf
+++ development/config.tf
@@ -1,3 +1,12 @@
 terraform {
   required_version = ">=0.12"
 }
+terraform {
+  backend "remote" {
+    organization = "EliteSolutionsIT"
+
+    workspaces {
+      name = "elite-TFC-cicd-aws-s3-static"
+    }
+  }
+}
